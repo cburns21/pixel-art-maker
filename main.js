77e7517a3f4f09017c4fd9026f953c6e23333be8
caroline burns <carolineburns@carolines-MacBook.local>
2018-11-28 11:01:25 -0700

    submitting for feedback

---
+++ main.js
@@ -0,0 +1,13 @@
+var container = document.getElementsByClassName('container')
+var grid= document.getElementById('canvas')
+for(let i=0; i < 60; i++){
+var row = document.createElement('div')
+// var column = document.createElement('div')
+grid.classList.add('canvas', 'container')
+document.row.appendChild('grid')
+document.container.appendChild('grid')
+
+// column.appendChild()
+}
+
+
